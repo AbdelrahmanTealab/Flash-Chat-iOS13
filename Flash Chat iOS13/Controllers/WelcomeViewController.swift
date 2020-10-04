316824f07b3edc05640e97153f70d5f4f9b66e39
Abdelrahman Tealab <62073015+AbdelrahmanTealab@users.noreply.github.com>
2020-10-03 22:43:28 -0400

    iOS chat app with CLTypingLabel pod

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -14,9 +14,14 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        titleLabel.text = ""
+        var charIndex = 0.0
 
-       
+        for letter in "⚡️FlashChat"{
+
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in self.titleLabel.text?.append(letter)
+            }
+            charIndex+=1
+        }
     }
-    
-
 }
